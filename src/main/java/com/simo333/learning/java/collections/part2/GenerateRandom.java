030ccc0fab3fe80142828592713fb5e51014677b
simo333 <szymon.333@wp.pl>
2022-05-17 22:08:16 +0200

    Added new task in com.simo333.learning.collections.par2

---
+++ src/main/java/com/simo333/learning/java/collections/part2/GenerateRandom.java
@@ -0,0 +1,18 @@
+package com.simo333.learning.java.collections.part2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class GenerateRandom {
+    public static Map<Integer, Integer> checkRand(int amount, int interval) {
+        Map<Integer, Integer> mapRandom = new HashMap<>();
+        Random random = new Random();
+        for (int i = 0; i < amount; i++) {
+            int newRandom = random.nextInt(interval);
+            mapRandom.putIfAbsent(newRandom, 1);
+            mapRandom.computeIfPresent(newRandom, (k, v) -> v + 1);
+        }
+        return mapRandom;
+    }
+}
