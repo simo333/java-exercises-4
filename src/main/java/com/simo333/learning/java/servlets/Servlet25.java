e008f669ff0266010c0c056d1d0655edf09c0cb4
simo333 <szymon.333@wp.pl>
2022-05-19 22:54:01 +0200

    Added tasks in com.simo333.learning.servlets

---
+++ src/main/java/com/simo333/learning/java/servlets/Servlet25.java
@@ -0,0 +1,44 @@
+package com.simo333.learning.java.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Random;
+
+@WebServlet(name = "Servlet25", value = "/servlet25")
+public class Servlet25 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Integer> ints = randomNumbers();
+        List<Integer> sortedInts = sortIntegerList(ints);
+        PrintWriter writer = response.getWriter();
+        for (Integer anInt : ints) {
+            writer.println(anInt);
+        }
+
+        writer.println();
+
+        for (Integer sortedInt : sortedInts) {
+            writer.println(sortedInt);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    List<Integer> randomNumbers() {
+        return new Random().ints(10, 1, 101)
+                .boxed().toList();
+    }
+
+    List<Integer> sortIntegerList(List<Integer> list) {
+        return list.stream().sorted().toList();
+    }
+}
